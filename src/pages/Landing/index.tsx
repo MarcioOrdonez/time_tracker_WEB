ddc65d4d297cdfdba3e83c9be4874b1d1ba23624
marcio.v.ordonez@gmail.com <marcio.v.ordonez@gmail.com>
2020-08-17 21:26:04 -0300

    still need fixes

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import logo from '../../assets/images/clock.png';
 
-function Landing(){
+import './styles.css';
+
+function Landing() {
     return (
-        <h1>Time tracker landing page</h1>
+        <div id="page-landing">
+            <div id="page-landing-conttent" className="container">
+                <div className="logo-container">
+                    <img src={logo} alt="Logo that represents a clock" />
+                    <h1>Your favourit time tracker to keep your time tracked</h1>
+                </div>
+                <img src={logo} alt="Logo that represents a clock" className="hero-image" />
+                <div className="buttons-container">
+                    <Link to="/login" className="login">
+                    Log in
+                </Link>
+
+                    <Link to="/register" className="register">
+                    Register
+                </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
