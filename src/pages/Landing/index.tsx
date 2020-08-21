eb12a59640b76e8113314298823e5be3dc2039d1
marcio.v.ordonez@gmail.com <marcio.v.ordonez@gmail.com>
2020-08-20 22:17:06 -0300

    landing page css

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '../../assets/images/clock.png';
+import logo from '../../assets/images/logo.svg';
+import heroImage from '../../assets/images/hero-image/hero-image.svg';
 
 import './styles.css';
 
 function Landing() {
     return (
         <div id="page-landing">
-            <div id="page-landing-conttent" className="container">
+            <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logo} alt="Logo that represents a clock" />
-                    <h1>Your favourit time tracker to keep your time tracked</h1>
+                    <h1>Make the most of your time.</h1>
                 </div>
-                <img src={logo} alt="Logo that represents a clock" className="hero-image" />
+                <img src={heroImage} alt="Logo that represents a clock" className="hero-image" />
                 <div className="buttons-container">
-                    <Link to="/login" className="login">
-                    Log in
+                    <Link to="/register" className="register">
+                    Join Now
                 </Link>
 
-                    <Link to="/register" className="register">
-                    Register
+                    <Link to="/login" className="login">
+                    Log in
                 </Link>
                 </div>
             </div>
